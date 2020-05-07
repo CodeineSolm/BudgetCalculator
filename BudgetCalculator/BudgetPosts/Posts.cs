963e8f7c852fddc504096b9bb1dc6819a6a83270
Max <bloodmailvol1@gmail.com>
2020-05-07 20:52:11 +0300

    Добавил библиотеку классов и класс Posts

---
+++ BudgetCalculator/BudgetPosts/Posts.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetPosts
+{
+    public class Posts
+    {
+        public int Id { get; set; }
+        public double Money { get; set; }
+        public string Discription { get; set; }
+    }
+}
