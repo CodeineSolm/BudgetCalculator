22a871073f4c15027c18bda23c07e522ecb5bfd5
Max <bloodmailvol1@gmail.com>
2020-05-07 20:54:16 +0300

    Добавил дату в класс Posts

--- BudgetCalculator/BudgetPosts/Posts.cs
+++ BudgetCalculator/BudgetPosts/Posts.cs
@@ -1,9 +1,12 @@
-﻿namespace BudgetPosts
+﻿using System;
+
+namespace BudgetPosts
 {
     public class Posts
     {
         public int Id { get; set; }
         public double Money { get; set; }
         public string Discription { get; set; }
+        public DateTime Date { get; set; }
     }
 }
