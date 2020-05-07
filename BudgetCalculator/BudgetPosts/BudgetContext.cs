bc7452b2553988adfcad72a63ea757269e03698d
Max <bloodmailvol1@gmail.com>
2020-05-07 20:59:35 +0300

    Добавил Context для нашей базы данных

---
+++ BudgetCalculator/BudgetPosts/BudgetContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace BudgetPosts
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext() : base("BCConnection")
+        {
+
+        }
+        DbSet<Posts> Posts { get; set; }
+    }
+}
