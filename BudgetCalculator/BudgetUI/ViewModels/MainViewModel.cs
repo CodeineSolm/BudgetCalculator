b8d01264993a6235790d19e4e430cf62b4432749
Max <bloodmailvol1@gmail.com>
2020-05-23 19:16:40 +0300

    Обновление
    Главное окно программы теперь пустое, добавил 2 страницы, которые активируются при нажатии кнопок в меню, одна страница с подсчетом денег, вторая пока пустая

--- BudgetCalculator/BudgetUI/ViewModels/MainViewModel.cs
+++ BudgetCalculator/BudgetUI/ViewModels/MainViewModel.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BudgetUI.ViewModels;
+using BudgetUI.Views;
 
 namespace BudgetUI.ViewModels
 {
     public class MainViewModel : Conductor<object>
     {
-
+        public void MoneyPage()
+        {
+            ActivateItem(new MoneyPageViewModel());
+        }        
+        public void SpendingsPage()
+        {
+            ActivateItem(new SpendingsPageViewModel());
+        }
     }
 }
