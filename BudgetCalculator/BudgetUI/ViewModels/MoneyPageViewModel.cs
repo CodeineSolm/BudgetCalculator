407ae967bacd5d78ced99a0e8079c0512b0c9ea0
Max <bloodmailvol1@gmail.com>
2020-05-27 15:52:33 +0300

    Кнопка record записывает в базу данных
    добавил свойства для Money и Discription, при нажатии эти свойства записываются в базу данных

--- BudgetCalculator/BudgetUI/ViewModels/MoneyPageViewModel.cs
+++ BudgetCalculator/BudgetUI/ViewModels/MoneyPageViewModel.cs
@@ -4,11 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BudgetPosts;
+using System.Windows.Controls;
+using BudgetUI.Views;
 
 namespace BudgetUI.ViewModels
 {
     public class MoneyPageViewModel : Screen
     {
-
+        private string _money;
+        public string Money
+        {
+            get 
+            { 
+                return _money; 
+            }
+            set 
+            { 
+                _money = value;
+                NotifyOfPropertyChange(() => Money);
+            }
+        }
+        private string _discription;
+        public string Discription
+        {
+            get 
+            { 
+                return _discription;
+            }
+            set
+            { 
+                _discription = value;
+                NotifyOfPropertyChange(() => Discription);
+            }
+        }
+        public Posts Post { get; set; }
+        public void recordClick()
+        {
+            Post = new Posts();
+            Post.Money = Convert.ToDouble(Money);
+            Post.Discription = Discription;
+            Post.Date = DateTime.Now;
+            Money = "";
+            Discription = "";
+        }
     }
 }
